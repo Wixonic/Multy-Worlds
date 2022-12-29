aea9ce96c0c47c1a30ea38c4e40639d80f105cc4
Wixonic <public.wixonic@icloud.com>
2022-12-29 23:37:21 +0100

    Updating deploy commands

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,7 @@
-gcloud run deploy ws --region=asia-northeast1 --source=app
-gcloud run deploy ws --region=europe-west1 --source=app
-gcloud run deploy ws --region=us-west1 --source=app
+gcloud builds submit --pack image=gcr.io/multy-worlds/ws app
+gcloud run deploy ws --region=asia-northeast1 --image=gcr.io/multy-worlds/ws
+gcloud run deploy ws --region=europe-west1 --image=gcr.io/multy-worlds/ws
+gcloud run deploy ws --region=us-west1 --image=gcr.io/multy-worlds/ws
 firebase deploy --only=hosting:multy-worlds
 
 gcloud run deploy ws-beta --region=europe-west1 --source=beta
