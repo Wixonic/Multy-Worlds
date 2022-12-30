2d397f0faecc87c37157516ed544500b422bf0d4
Wixonic <public.wixonic@icloud.com>
2022-12-30 19:10:56 +0100

    Adding cleaning commands

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,14 @@
+# APP
 gcloud builds submit --pack image=gcr.io/multy-worlds/ws app
 gcloud run deploy ws --region=asia-northeast1 --image=gcr.io/multy-worlds/ws
 gcloud run deploy ws --region=europe-west1 --image=gcr.io/multy-worlds/ws
 gcloud run deploy ws --region=us-west1 --image=gcr.io/multy-worlds/ws
 firebase deploy --only=hosting:multy-worlds
 
+# BETA
 gcloud run deploy ws-beta --region=europe-west1 --source=beta
 firebase deploy --only=hosting:beta-multy-worlds
+
+# CLEAN
+gcloud storage rm gs://artifacts.multy-worlds.appspot.com/**
+gcloud storage rm gs://multy-worlds_cloudbuild/**
