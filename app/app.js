24b763632ce1db9aca29aa923005d0a161df5c9a
Wixonic <public.wixonic@icloud.com>
2023-01-21 17:21:49 +0100

    Removing old app

--- app/app.js
+++
@@ -1,5 +0,0 @@
-const PORT = parseInt(process.env.PORT) || 8080;
-const server = require("http").createServer();
-const io = new (require("socket.io").Server)(server);
-
-server.listen(PORT,() => console.info(`Server: listening on port ${PORT}`));
